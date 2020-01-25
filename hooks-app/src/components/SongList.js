416b7f08d9cac00137ad2bde679fd43ec8445efd
aldrin163 <aldrinjenson@gmail.com>
2020-01-25 21:26:26 +0530

    completed hooks app

--- hooks-app/src/components/SongList.js
+++ hooks-app/src/components/SongList.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState,useEffect} from "react";
 import uuid from 'uuid/v1'
 import NewSongForm from "./NewSongForm";
 
@@ -12,11 +12,23 @@ const SongList = () => {
         {title:'This Wild Darkness', id:2}
     ])
 
+    const [age,setAge] = useState(20)
+
     const addSong = (title) =>{
         setSongs([...songs,{ title, id: uuid() }])
     }
 
+    // like the render method, this hook runs whenever the components renders
+    useEffect(()=>{
+      console.log('useEffect hook running for ', songs);
+    },[songs])
+    // this hook will run now only when the data of the songs list changes and not when the age is changed
+
     // The useState hook returns the songs array and a function to modify the state
+
+    useEffect(()=>{
+      console.log('useEffect hook running',age);
+    },[age])
 
   return (
     <div className="song-list">
@@ -27,6 +39,7 @@ const SongList = () => {
       </ul>
       {/* <button onClick={addSong}>Add new Song</button> */}
       <NewSongForm addSong={addSong} />
+      <button onClick={()=>setAge(age+1)} >Add {age} by 1</button>
     </div>
   );
 };
